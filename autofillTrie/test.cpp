acaed8546d65378efbd32e011e7a67f925a73619
Salsasp <dakotagee99@gmail.com>
2023-09-08 16:44:19 -0500

    functional

--- autofillTrie/test.cpp
+++ autofillTrie/test.cpp
@@ -25,7 +25,7 @@ public:
             {
                 if(n->children[i] != NULL) trieStack.push(n->children[i]);
             }
-            if(n->isEndOfWord)cout<<n->currentLetters;
+            if(n->isEndOfWord)cout<<n->currentLetters<<endl;
         }
     }
 };
@@ -41,16 +41,23 @@ public:
 
     //TODO Test this function to see if you can get the right input
     void insert(string word) {
+        string currLetters = "";
+        bool isNew;
         Node* current = root;
         for (char c : word) {
+            isNew = false;
             int index = c - 'a';
             //make sure to get rid of case sensitivity
             if(index < 0){
                 index += 32;
             }
+            currLetters += c;
             if (!current->children[index]) {
                 current->children[index] = new Node();
+                isNew = true;
             }
+            if(isNew)current->children[index]->currentLetters = currLetters;
+            else current->currentLetters = currLetters;
             current = current->children[index];
         }
         current->isEndOfWord = true;
@@ -58,20 +65,25 @@ public:
 
     void findAllWords(string str)
     {
-        Node n = findNode(str);
+        Node* n = findNode(str);
         if(n == NULL)return;
-        n.validWords();
+        n->validWords();
     }
     
-    Node findNode(string str)
+    Node* findNode(string str)
     {
         Node* n = root;
+        int index;
         for(int i = 0; i < str.length(); i++)
         {
-            n = n->children[str[i] - 'a'];
-            if(n == nullptr) return NULL;
+            index = str[i] - 'a';
+            if(index < 0){
+                index += 32;
+            }
+            n = n->children[index];
+            if(n == nullptr) return nullptr; //temp fix change this later
         }
-        return *n;
+        return n;
     }
 };
 
@@ -87,9 +99,11 @@ int main()
         cin>>word;
         trie.insert(word);
     }
-    //TODO run search functionality here
-
     cout << "Print Tester" << endl;
+    if(trie.findNode("AMD"))cout << "AMD is in dict!" << endl;
+    if(trie.findNode("AM"))cout << "AM is in dict!" << endl;
+    if(trie.findNode("zqr"))cout << "zqr is in dict!" << endl;
+    trie.findAllWords("str");
     return 0;
 }
 
