eb740dd3cd3bdefdf96bb64164f7e228ac29820c
Salsasp <dakotagee99@gmail.com>
2023-09-17 00:28:25 -0500

    changes

--- Hidden Words/hiddenwords.cpp
+++ Hidden Words/hiddenwords.cpp
@@ -2,34 +2,38 @@
 using namespace std;
 bool found = false;
 
-bool isAdjacent(pair<int,int> prevPos, pair<int,int> newPos)
+bool isAdjacent(pair<int,int> prevPos, pair<int,int> newPos, set<pair<int,int>>& visitedPos)
 {
-    //if statements check if next letter is one away from previous letter
-    if((prevPos.first + 1 == newPos.first && prevPos.second == newPos.second) || (prevPos.first -1 == newPos.first && prevPos.second == newPos.second) ||
-                (prevPos.second + 1 == newPos.second && prevPos.first == newPos.first) || (prevPos.second - 1 == newPos.second && prevPos.first == newPos.first))
-                {
-                    return true;
-                }
+    //if statements check if next letter is one away from previous letter and make sure that a previously visited letter cannot be visited again
+    if((prevPos.first + 1 == newPos.first && prevPos.second == newPos.second && !visitedPos.count(newPos)) || 
+        (prevPos.first -1 == newPos.first && prevPos.second == newPos.second && !visitedPos.count(newPos)) ||
+        (prevPos.second + 1 == newPos.second && prevPos.first == newPos.first && !visitedPos.count(newPos)) || 
+        (prevPos.second - 1 == newPos.second && prevPos.first == newPos.first && !visitedPos.count(newPos)))
+        {
+            visitedPos.insert(newPos);
+            return true;
+        }
     return false;
 }
 
-void findWord(char* grid[], unordered_multimap<char, pair<int,int>> letterPosMap, string target, pair<int,int> prevPos, int& inGrid, bool isFirstPassthrough, int strIndex)
+void findWord(unordered_multimap<char, pair<int,int>> letterPosMap, string target, pair<int,int> prevPos, int& inGrid, bool isFirstPassthrough, int strIndex, set<pair<int,int>> visitedPos)
 {
     if(isFirstPassthrough){
         found = false;
         for(auto it = letterPosMap.equal_range(target[0]).first; it != letterPosMap.equal_range(target[0]).second; it++) //loop through all occurences of first letter in grid
         {
-            if(target[strIndex] == target[target.size()-1]){found = true; inGrid++; return;}//for the case in which a 1 letter word exists
-            findWord(grid, letterPosMap, target, it->second, inGrid, false, strIndex+1);
+            if(strIndex == target.size()-1){found = true; inGrid++; return;}//for the case in which a 1 letter word exists
+            visitedPos.insert(it->second); //put first letter pos in set
+            findWord(letterPosMap, target, it->second, inGrid, false, strIndex+1, visitedPos);
         }
     }
-    if(found)return; //base case to exit recursion if the word has been found in other function calls; uses global variable (admittedly a bad practice)
     for(auto it = letterPosMap.equal_range(target[strIndex]).first; it != letterPosMap.equal_range(target[strIndex]).second; it++)//loop through all occurences of next letter in word
     {
-        if(isAdjacent(prevPos, it->second))
+        if(found)return; //base case to exit recursion if the word has been found in other function calls; uses global variable (admittedly a bad practice)
+        if(isAdjacent(prevPos, it->second, visitedPos))
             {
-                if(target[strIndex] == target[target.size()-1]){found = true; inGrid++; return;}//if the last letter matches, return and increment inGrid
-                findWord(grid, letterPosMap, target, it->second, inGrid, false, strIndex+1);
+                if(strIndex == target.size()-1){found = true; inGrid++; return;}//if on last letter, return and increment inGrid
+                findWord(letterPosMap, target, it->second, inGrid, false, strIndex+1, visitedPos);
             }
     }
 }
@@ -40,19 +44,18 @@ int main()
     freopen("test_input.txt", "r", stdin);
     int width, height, numTargetWords, numInGrid;
     unordered_multimap<char, pair<int,int>> letterPosMap;
+    set<pair<int,int>> visitedPos;
     string tempWord;
-    cin >> width; cin >> height;
-    char *wordGrid[width];
-    for(int i = 0; i < width; i++)
-    {
-        wordGrid[i] = new char[height]; //2d array using pointers
+    cin >> height; cin >> width;
+    if(width == 0 || height == 0){ //return 0 and exit if either width or height is 0
+        cout << 0;
+        return 0;
     }
-    for(int i = 0; i < height; i++) //loop to read input into 2d char matrix
+    for(int i = 0; i < height; i++) //loop to read input
     {
         cin >> tempWord;
         for(int j = 0; j < width; j++)
         {
-            wordGrid[i][j] = tempWord[j];
             char tempChar = tempWord[j];
             letterPosMap.insert(make_pair(tempChar, pair(i,j))); //hashmap to allow quick access to location of specific letters
         }
@@ -64,7 +67,8 @@ int main()
     for(int i = 0; i < numTargetWords; i++)
     {
         cin >> tempstr;
-        findWord(wordGrid, letterPosMap, tempstr, pair(0,0), numInGrid, true, 0);
+        findWord(letterPosMap, tempstr, pair(0,0), numInGrid, true, 0, visitedPos);
+        visitedPos.clear();
     }
     cout << numInGrid;
 }
